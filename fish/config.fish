2e4eb7f3390e3f7b0f46ee1ea62665568bbe8f6e
trapmagistr <glebbannikov201@gmail.com>
2026-08-02 15:35:10 +0300

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+	fastfetch
+    # Commands to run in interactive sessions can go here
+end
+
+# Added by LM Studio CLI tool (lms)
+set -gx PATH $PATH /home/zei/.lmstudio/bin
