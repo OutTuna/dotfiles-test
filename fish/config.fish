bd2edc80c1f57352fa1d704f514d002b764c0c95
Gleb <60158541+OutTuna@users.noreply.github.com>
2026-09-09 13:45:19 +0300

    chore(macos): remove legacy root configs

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-	fastfetch
-    # Commands to run in interactive sessions can go here
-end
-
-# Added by LM Studio CLI tool (lms)
-set -gx PATH $PATH /home/zei/.lmstudio/bin
